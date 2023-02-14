a97af1f62432e1460f688e637fc25a67bb20f8af
nakh2010 <nakh2010@gmail.com>
2023-02-14 09:33:35 +0700

    Update faq.js

---
+++ assets/faq.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(".lsn-js-faq-body:not(:first-of-type)").css("display", "none");
+    $(".lsn-faq-question:first-of-type").addClass("open");
+   
+    $(".lsn-faq-question").click(function () {
+      $(".open").not(this).removeClass("open").next().slideUp(300);
+      $(this).toggleClass("open").next().slideToggle(300);
+    });
+  });
